c4576e9b40d4f4083c3be833ca57d9fb315a38a9
Zane Milakovic <zane.milakovic@gmail.com>
2014-08-19 09:37:54 -0500

    Updates gulpfile to pass jshint linting
    Updates gulpfile to pass jshint linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    util = require('gulp-util');
+var gulp = require("gulp"),
+    autoprefixer = require("gulp-autoprefixer"),
+    minifycss = require("gulp-minify-css"),
+    jshint = require("gulp-jshint"),
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename"),
+    clean = require("gulp-clean"),
+    concat = require("gulp-concat"),
+    util = require("gulp-util");
 
 // Options
 var options = {
 	autoprefixer: {
 		map: true,
-		from: 'placeholder',
-		to: 'style.min.css'
+		from: "placeholder",
+		to: "style.min.css"
 	},
 	rename: {
-		suffix: '.min'
+		suffix: ".min"
 	},
 	clean: {
 		read: false
@@ -25,40 +25,51 @@ var options = {
 
 var paths = {
  	scripts: [
-	 	'src/*.js', 
-		'!src/vendor/*.js'
+	 	"src/*.js"
 	],
 	styles: [
-		'src/*.css'
+		"src/*.css"
 	]
 };
 
-gulp.task('scripts', function() {
+gulp.task("scripts", function() {
+	"use strict";
 	return gulp.src(paths.scripts)
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'))
-		.pipe(gulp.dest('./build'))
+        .pipe(jshint(".jshintrc"))
+        .pipe(jshint.reporter("jshint-stylish"))
+		.pipe(gulp.dest("./build"))
         .pipe(uglify())
 		.pipe(rename(options.rename))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest("./build"));
 });
 
-gulp.task('styles', function() {
+gulp.task("styles", function() {
+	"use strict";
     return gulp.src(paths.styles)
 		.pipe(autoprefixer(["last 1 version", "> 1%", "ie 8", "ie 7"], options.autoprefixer))
-		.pipe(gulp.dest('./build'))
+		.pipe(gulp.dest("./build"))
 		.pipe(minifycss())
 		.pipe(rename(options.rename))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest("./build"));
 });
 
-gulp.task('clean', function() {
-	return gulp.src('./build', options.clean)
+gulp.task("clean", function() {
+	"use strict";
+	return gulp.src("./build", options.clean)
 		.pipe(clean());
 });
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles');
-    gulp.start('scripts');
+gulp.task("default", ["clean"], function() {
+	"use strict";
+	gulp.start("watch");
 });
 
+gulp.task("watch", ["styles", "scripts"], function() {
+	"use strict";
+
+    // Watch .css files
+    gulp.watch(paths.styles, ["styles"]);
+
+    // Watch .js files
+    gulp.watch(paths.scripts, ["scripts"]);
+});
